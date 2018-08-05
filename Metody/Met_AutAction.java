3d6b658b837110b849b8e39a0db1ce79428764cb
KTK81 <jankotek@gmail.com>
2018-08-05 15:24:27 +0200

    autro akcia
    male zmeny v kode, skrz cenu v akcia subore, skrz identifikaciu ciarky a jej zmeny na bodku

--- Metody/Met_AutAction.java
+++ Metody/Met_AutAction.java
@@ -16,12 +16,21 @@ public class Met_AutAction {
         while (riadokAkcia != null) {
             riadokAkcia = citacAkcia.readLine();
             if (riadokAkcia != null) {
+                String priceAkciaString=null;
                 int bodkociarka = riadokAkcia.indexOf(";");
                 int bodkociarka2 = riadokAkcia.indexOf(";", bodkociarka+1);
                 int bodkociarka3 = riadokAkcia.indexOf(";", bodkociarka2+1);
                 int bodkociarka4 = riadokAkcia.indexOf(";", bodkociarka3+1);
                 String codeAkcia = riadokAkcia.substring(bodkociarka+1, bodkociarka2);
-                String priceAkciaString = ((riadokAkcia.substring(bodkociarka3+1, bodkociarka4)).replace(",",".")).trim();
+//                priceAkciaString = ((riadokAkcia.substring(bodkociarka3 + 1, bodkociarka4)).replace(",", "."));
+                priceAkciaString = ((riadokAkcia.substring(bodkociarka3 + 1, bodkociarka4)));
+                if (priceAkciaString.contains(",")) {
+                    priceAkciaString = priceAkciaString.replace(",", ".");
+                }
+                priceAkciaString.trim();
+//                System.out.print("kod:"+codeAkcia);
+//                System.out.println(";cena:"+priceAkciaString);
+
                 priceAkcia = new BigDecimal(priceAkciaString);
                 priceAkcia = priceAkcia.setScale(2);
                 autroAkcia.add (new Produkt(codeAkcia,priceAkcia));
