3efdb5883e590b54a8ac0062d76855a3e5235b8e
KTK81 <jankotek@gmail.com>
2018-10-16 23:40:00 +0200

    category update
    zhruba tempo kategorie, este dostelovat

--- Metody/Met_Category.java
+++ Metody/Met_Category.java
@@ -53,71 +53,27 @@ public class Met_Category {
                 }
             }
 
-//
-//            //KUCHYNA
-//            if (kategoriaOriginal.contains("Kuchyňa a Jedáleň > Stoličky > Drevené"))
-//                kategoria = "Drevené stoličky, Stoličky, Kuchyňa";
-//            if (kategoriaOriginal.contains("Kuchyňa a Jedáleň > Stoličky > Kovové"))
-//                kategoria = "Kovové stoličky, Stoličky, Kuchyňa";
-//            if (kategoriaOriginal.contains("Jedálenské sety"))
-//                kategoria = "Jedálenské sety, Kuchyňa";
-//            if (kategoriaOriginal.contains("Kuchyňa a Jedáleň > Kuchynské linky > Komplety"))
-//                kategoria = "Kompletné linky, Kuchynské linky, Kuchyňa";
-//            if (kategoriaOriginal.contains("Kuchyňa a Jedáleň > Lavice do kuchyne"))
-//                kategoria = "Lavice do kuchyne, Stoličky, Kuchyňa";
+            //OK, prebehol som zoznamy, teraz par indivudualne riesenych kategorii
 
-            //jedalenske stolicky, drevene vs kovove, tiez specificky treba riesit, ale lavice su vzdy lavice
+            //sedacky na mieru BORN nemaju v Tempe vlastnu podkategoriu, tak si ich musim oddelit z hlavnej kategorie
+            if ((kategoriaOriginal.contains("Sedačky na mieru"))&&(meno.contains("BORN"))) {
+                kategoria = "BORN, Sedačky na mieru, Sedacie súpravy, Obývačka";
+            }
+
+            //jedalenske stolicky, drevene vs kovove, tiez specificky treba riesit, ale lavice su vzdy lavice samostatne
             if ((kategoria.contains("Stoličky"))&& (!(kategoria.contains("Lavice")))){
                 if (popis.contains("kov"))
                     kategoria = "Kovové stoličky, Stoličky, Kuchyňa";
             }
-//
-//            //Kuchynske ZOSTAVY
-//            if (kategoriaOriginal.contains("Prado-biela lesk HG/korpus sivá"))
-//                kategoria = "Prado-biela lesk HG/korpus sivá, Zostavy na mieru, Kuchyňa";
-//            if (kategoriaOriginal.contains("Prado-krém lesk HG/sivá lesk HG/sivá"))
-//                kategoria = "Prado-krém lesk HG/sivá lesk HG/sivá, Zostavy na mieru, Kuchyňa";
-//            if (kategoriaOriginal.contains("Prado-krémová lesk HG/korpus sivá"))
-//                kategoria = "Prado-krémová lesk HG/korpus sivá, Zostavy na mieru, Kuchyňa";
-//            if (kategoriaOriginal.contains("Kuchyne na mieru > Vega"))
-//                kategoria = "Vega, Zostavy na mieru, Kuchyňa";
-//
-//            //SPALNA
-//            if (kategoriaOriginal.contains("Kompletné spálne"))
-//                kategoria = "Kompletné spálne, Spálňa";
-//            if (kategoriaOriginal.contains("Jednolôžka"))
-//                kategoria = "Jednolôžkové postele, Postele, Spálňa";
-//            if (kategoriaOriginal.contains("Kancelária > Regály"))
-//                kategoria = "Skrinky a regály, Kancelária";
 
             //postele su specificke, podla sirky postele sa urci kategoria
             if (kategoria.equals("Jednolôžkové postele, Postele, Spálňa")) {
                 if ((name.contains("140"))||(name.contains("160"))||(name.contains("180")))
                     kategoria = "Manželské postele, Postele, Spálňa";
             }
-//
-//            //OBYVACKA
-//            if (kategoriaOriginal.contains("Obývacia izba > Konferenčné stolíky"))
-//                kategoria = "Konferenčné stolíky, Stolíky, Obývačka";
-//            if (kategoriaOriginal.contains("Obývacia izba > TV stolíky"))
-//                kategoria = "TV stolíky, Stolíky, Obývačka";
-//            if (kategoriaOriginal.contains("Rohové sedačky"))
-//                kategoria = "Rohové súpravy, Sedacie súpravy, Obývačka";
-//            if (kategoriaOriginal.contains("Obývačka > Sedačky a sedacie súpravy > Sedačky v tvare U"))
-//                kategoria = "Sedačky v tvare U, Sedacie súpravy, Obývačka";
-//            if (kategoriaOriginal.contains("Obývačka > Pohovky"))
-//                kategoria = "Pohovky, Sedacie súpravy, Obývačka";
-//
-//
-//            //PREDSIEN
-//            if (kategoriaOriginal.contains("Predsieň > Lavice a taburety"))
-//                kategoria = "Lavice, Predsieň";
 
 
-
-//                    break;
             }
-//        }
 
         return kategoria;
     }
