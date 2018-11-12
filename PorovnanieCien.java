5e5d0915b53c08fc9bd1d1e3ae62fe12b160dea0
KTK81 <jankotek@gmail.com>
2018-11-12 08:57:32 +0100

    Porovnanie cien - autronic sedaky
    Bol to boj, ale malo by to byt OK. Ak mam sedacku so sedakom, pribudol stlpec, kde je suma, za kolko by sa to malo po novom predavat - bez sedaka.

--- PorovnanieCien.java
+++ PorovnanieCien.java
@@ -1,9 +1,11 @@
+import Metody.Met_Price;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DecimalFormat;
@@ -20,7 +22,7 @@ public class PorovnanieCien {
         ArrayList<Produkt> prestaNase = new ArrayList<>();
         prestaNase = PrestaIDRead.filePresta();
         PrintWriter writerSubor = new PrintWriter(Premenne.cesta + "porovnanieCien.csv", "UTF-8");
-        writerSubor.println("PrestaID;Kod;Nasa cena;Spravna cena;Rozdiel;Vyrobca;Stav");
+        writerSubor.println("PrestaID;Kod;Nasa cena;Spravna cena;Rozdiel;Spravna cena bez sedaka;Vyrobca;Stav");
 
          for (int i = 0; i < prestaNase.size(); i++) {
 //             if (prestaNase.get(i).getAktivita().contains("1")) {
@@ -31,27 +33,37 @@ public class PorovnanieCien {
                 String kod = prestaNase.get(i).getKod();
                 String kategoria = prestaNase.get(i).getSkupina();
                 String cenaNasa = prestaNase.get(i).getMOC();
-                Double cenaNasaDouble = Double.parseDouble(cenaNasa);
-                Double spravnaCena = najdiCenu(kod);
-                Double rozdiel = spravnaCena-cenaNasaDouble;
+                BigDecimal cenaNasaBezSedaka = new BigDecimal(cenaNasa);
+                //ak su sedaky na stolicke. Tak musim spocitat cenu za stolicku a cenu sedaku, aby som mal konecnu cenu
+                 if (vyrobca.contains("AUTRONIC"))
+                     cenaNasa = Met_Price.cenaAutronicSedaky(cenaNasa, kod);
+
+                BigDecimal cenaNasaBigDecimal = new BigDecimal(cenaNasa);//bud so sedakom, alebo ak nema sedak, tak povodna
+                BigDecimal spravnaCena = najdiCenu(kod); //cena u konkurencie, mala by byt ta spravna cena
+                BigDecimal rozdiel = cenaNasaBigDecimal.subtract(spravnaCena); //rozdiel medzi nasou a spravnou. plusova hodnota, ak mam znizit, minusova, ak mam zvysit
+                BigDecimal rozdielBezSedaka = cenaNasaBezSedaka.subtract((rozdiel)); //ak ma stolicka sedak, tak musim pozriet na cenu predtym, ako som
+                 // k tej cene pripocital sedak a z tejto sumy musim odobrat rozdiel a ten vysledok, to je suma, za ktoru by som ju mal predavat, bez sedaka
+                 //Ked k tejto vyslednej sume pripocitam cenu za sedak, bude taka, ako ma byt
+                 //Kedze do Presty viem cez import CSV-cka zadat len tu cenu bez sedaku, tak ta ma musi zaujimat, tu chcem zmenit
                 String stav = null;
 //                System.out.print("PrestaID:"+prestID+";Kod:"+kod+";Nasa cena:"+cenaNasaDouble+";Spravna cena:"+spravnaCena+";Rozdiel:"+rozdiel+";Vyrobca:"+vyrobca);
-                if (cenaNasaDouble.equals(spravnaCena)) {
+
+                if (cenaNasaBigDecimal.equals(spravnaCena)) {
                     stav = "rovnake ceny";
                 }
                 else {
                     stav = "INE ceny";
                 }
-                 System.out.println(kod+";"+cenaNasaDouble+";"+spravnaCena+";"+rozdiel+";"+stav);
-                 writerSubor.println(prestID+";"+kod+";"+cenaNasaDouble+";"+spravnaCena+";"+rozdiel+";"+vyrobca+";"+kategoria+";"+stav);
+                 System.out.println(kod+";"+cenaNasaBigDecimal+";"+spravnaCena+";"+rozdiel+";"+rozdielBezSedaka+";"+stav);
+                 writerSubor.println(prestID+";"+kod+";"+cenaNasaBigDecimal+";"+spravnaCena+";"+rozdiel+";"+rozdielBezSedaka+";"+vyrobca+";"+kategoria+";"+stav);
                 Thread.sleep(500);
             }
         }
         writerSubor.close();
     }
 
-    public static Double najdiCenu(String kod) throws IOException {
-        Double cenaDouble = 7777.7;
+    public static BigDecimal najdiCenu(String kod) throws IOException {
+        BigDecimal cenaDouble = new BigDecimal(77777.00);
         try {
                 String kodNaHladanie = kod.replaceAll("\\s", "+");
                 URL hladanie = new URL("https://www.lacnyeshop.sk/lacnyeshop/e-search?q="+kodNaHladanie+"&qm=2");
@@ -68,7 +80,7 @@ public class PorovnanieCien {
                         int koniec = riadokArtium.lastIndexOf("span");
                         cena = riadokArtium.substring(zaciatok + 20, koniec - 9);
                         cena = cena.replaceAll(",", ".");
-                        cenaDouble = Double.parseDouble(cena);
+                        cenaDouble = new BigDecimal(cena);
                         break;
                     }
                 }
